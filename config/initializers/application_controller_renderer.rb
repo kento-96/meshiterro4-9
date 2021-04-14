5309eedef47983417894b8ebab3775363226f365
kento-96 <kento.soccer.1107@gmail.com>
2021-04-14 08:39:28 +0000

    [Finish]4-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '4410744c05b14bb73b7728ffa2f09123c774bfde7b17c54bda0c62c3f970ecf9eb268a0534c4cbf974ff89bdf43a6cbd705c934db98cded121c849d691b3f401'
